d998501eb4f6fa0de2947971e5a9960a86c5cbfd
Li YouHua <2973365069@qq.com>
2026-03-13 16:04:03 +0800

    Create client_3.py file that implements Client 3 for federated learning

---
+++ backend/clients/client_3.py
@@ -0,0 +1,22 @@
+# Client 3 Implementation for Federated Learning
+
+class Client3:
+    def __init__(self, ...):
+        # Initialization code
+        pass
+
+    def train(self):
+        # Training logic
+        pass
+
+    def evaluate(self):
+        # Evaluation logic
+        pass
+
+    def send_model(self):
+        # Logic to send model to server
+        pass
+
+    def receive_model(self):
+        # Logic to receive model from server
+        pass
