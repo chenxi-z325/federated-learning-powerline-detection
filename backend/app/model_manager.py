36e8a59e771ab0dc922d2c8a401c63863b9a55a3
Li YouHua <2973365069@qq.com>
2026-03-13 16:01:47 +0800

    Create model_manager.py for YOLOv11n model management

---
+++ backend/app/model_manager.py
@@ -0,0 +1,21 @@
+# model_manager.py
+
+class ModelManager:
+    def __init__(self, model_path):
+        self.model_path = model_path
+
+    def load_model(self):
+        # Code to load the YOLOv11n model from self.model_path
+        pass
+
+    def save_model(self):
+        # Code to save the YOLOv11n model to self.model_path
+        pass
+
+    def predict(self, image):
+        # Code to run inference on an image using the YOLOv11n model
+        pass
+
+    def evaluate(self, dataset):
+        # Code to evaluate the model on a dataset
+        pass
